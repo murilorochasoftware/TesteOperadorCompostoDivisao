486d88b45ece8e875303c2f64a8c4a9026c55fd1
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 20:00:26 -0300

    Teste do uso do operador de atribuição de maneira composta com o operador de divisão em Java.

---
+++ src/TesteOperadorCompostoDivisao.java
@@ -0,0 +1,9 @@
+public class TesteOperadorCompostoDivisao {
+
+    public static void main(String[] args) {
+        float lucro = 125.00f;
+        lucro /= 2;
+
+        System.out.printf("Lucro R$: %.2f", lucro);
+    }
+}
